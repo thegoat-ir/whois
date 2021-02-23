0593bce7b39c82389c09dabfd954c1c7b702676c
Mahdyar <hi@mahdyar.me>
2021-02-23 08:51:53 +0330

    feat: add github repository link

--- components/copyright.vue
+++ components/copyright.vue
@@ -1,5 +1,11 @@
 <template>
   <footer>
+    <a
+      href="https://github.com/thegoat-ir/whois"
+      alt="The Goat's GitHub"
+      target="_blank"
+      ><github-logo
+    /></a>
     <p>
       Made with &lt;3 by
       <a href="https://thegoat.ir" alt="The Goat" target="_blank">The Goat</a>
@@ -7,15 +13,23 @@
   </footer>
 </template>
 <style scoped>
-footer {
+svg {
+  position: absolute;
+  display: inline-block;
+  bottom: 3rem;
+  transform: translate(5%, 0);
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+p {
   width: 85%;
   position: absolute;
-  display: block;
+  display: inline-block;
   text-align: center;
   color: #59405d;
   left: 50%;
   transform: translate(-50%, 0);
-  bottom: 2rem;
+  bottom: 1rem;
 }
 a {
   color: inherit;
